4dbe605f9366f56608b06259b2e84b8d046d5145
карина <bik_k.bio@inbox.ru>
2025-07-24 19:05:29 +0300

    ввава

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,6 @@ app.post('/register', (req, res) => {
 });
 
 // 4. Запуск сервера
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(5500, () => {
+  console.log('Server running on port 5500');
 });
